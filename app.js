f22604abd9989af2b890a1c9a2e0192df840fba3
darksim735 <darksim735@gmail.com>
2015-02-10 21:15:39 -0500

    template renders title based on variable passed

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var swig = require('swig');
 
 var logger = require('morgan');
 app.use(logger('dev'));
 
 app.listen(3000);
 
+//swig.renderFile('/Users/randallwong/Desktop/FoundationsPrework/twitter-js/views/index.html', {title: 'APP SET TITLE INSERTION!'});
+//app.engine(swig.renderFile());
+
+//app.set('title', 'APP SET TITLE INSERTION!');
+
 app.get('/', function(req, res){
-    res.send('Hello World! Updated live!!!');
+    //res.send('Hello World! Updated live!!!');
+
+res.send(swig.renderFile('/Users/randallwong/Desktop/FoundationsPrework/twitter-js/views/index.html', {title: 'APP SET TITLE INSERTION!'}));
+
 });
