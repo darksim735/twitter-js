fdf4a2b8c25dc17e72660ea09b4a63aad833416b
darksim735 <darksim735@gmail.com>
2015-02-10 21:24:50 -0500

    we set caching to false, still need to read up on passing in the variables outside of renderFile

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ app.use(logger('dev'));
 
 app.listen(3000);
 
+swig.setDefaults({ cache: false });
+
 //swig.renderFile('/Users/randallwong/Desktop/FoundationsPrework/twitter-js/views/index.html', {title: 'APP SET TITLE INSERTION!'});
 //app.engine(swig.renderFile());
 
 //app.set('title', 'APP SET TITLE INSERTION!');
 
+
+//swig.set('title', 'APP SET TITLE INSERTION!');
+
 app.get('/', function(req, res){
     //res.send('Hello World! Updated live!!!');
 
 res.send(swig.renderFile('/Users/randallwong/Desktop/FoundationsPrework/twitter-js/views/index.html', {title: 'APP SET TITLE INSERTION!'}));
+
+
+//res.send(swig.renderFile('/Users/randallwong/Desktop/FoundationsPrework/twitter-js/views/index.html'));
+
 
 });
